1cfc4fa232285473ca2fec2e8d8811f28eaf3cb3
Oscuro87 <capelle.e@gmail.com>
2022-10-26 01:00:16 +0200

    Write test for shortcut fn

--- src/tests/weather.rs
+++ src/tests/weather.rs
@@ -3,6 +3,7 @@ use crate::owm::weather::structures::WeatherData;
 use crate::owm::weather::api::get_weather_for_coordinates;
 use crate::owm::geocoding::structures::Coordinates;
 use crate::owm::geocoding::api::get_coordinates_by_location_name;
+use crate::get_weather_by_city;
 
 #[tokio::test]
 pub async fn attempt_weather_data_retrieval() {
@@ -10,5 +11,13 @@ pub async fn attempt_weather_data_retrieval() {
     let credentials = read_credentials();
     let coords: Coordinates = get_coordinates_by_location_name(city_name, credentials.omw_api_key.clone()).await;
     let weather: WeatherData = get_weather_for_coordinates(coords.coord.lat, coords.coord.lon, credentials.omw_api_key).await;
+    println!("{:#?}", weather);
+}
+
+#[tokio::test]
+pub async fn attempt_weather_data_retrieval_with_shortcut_fn() {
+    let city_name: String = "Brussels".into();
+    let credentials = read_credentials();
+    let weather: WeatherData = get_weather_by_city(city_name, credentials.omw_api_key).await;
     println!("{:#?}", weather);
 }
