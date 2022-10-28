fbe11566f154288ef07459638f9aa0b9225fab52
Oscuro87 <capelle.e@gmail.com>
2022-10-28 21:35:44 +0200

    Rewrite API and use constants

--- src/owm/geocoding/api.rs
+++ src/owm/geocoding/api.rs
@@ -1,13 +1,10 @@
 #![allow(dead_code)]
 
 use crate::owm::geocoding::structures::Coordinates;
+use crate::owm::constants::{OWM_URI, ENDPOINT_WEATHER};
 
 pub async fn get_coordinates_by_location_name(city_name: String, api_key: String) -> Coordinates {
-    let call: String = format!(
-        "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}", 
-        city_name, 
-        api_key
-    );
+    let call: String = format!("{OWM_URI}{ENDPOINT_WEATHER}?q={city_name}&appid={api_key}");
     let response: reqwest::Response = reqwest::get(&call).await.expect("Error");
     response.json().await.expect("Could not parse the JSON.")
 }
